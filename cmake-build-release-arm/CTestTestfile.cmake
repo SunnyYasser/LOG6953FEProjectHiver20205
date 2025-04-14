f954c41c898b430572b3265050611b1d69a403d6
Anas Dorbani <anasdorbani@gmail.com>
2025-04-14 08:31:05 -0400

    added the arm binary

---
+++ cmake-build-release-arm/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anas/Documents/LOG6953FEProjectHiver20205
+# Build directory: /Users/anas/Documents/LOG6953FEProjectHiver20205/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src/data")
+subdirs("src/graph")
+subdirs("src/operator")
+subdirs("src/memory")
+subdirs("src/engine")
+subdirs("src/debug")
+subdirs("src/utils")
+subdirs("src/parser")
+subdirs("tests")
